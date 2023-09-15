88fda0b9176df98ff096c4c9deb198ea88f82745
chandra swaroop <chandraswaroop6630@gmail.com>
2023-09-16 00:10:01 +0530

    8

---
+++ P9.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class P9 {
+	public static void main(String[] args) {
+		for (int i = 1; i <=4; i++) {
+			for (int j = 1; j <=4; j++) {
+				if (i==2&&j==2||i==2&&j==4||i==3&&j==2||i==3&&j==4) {
+					System.out.print("  ");
+				}
+				else {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
