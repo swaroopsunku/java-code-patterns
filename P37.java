4221c02ab2a8e7ccf0bf64e05b2b5c51e7532ae5
chandra swaroop <chandraswaroop6630@gmail.com>
2023-09-27 10:32:45 +0530

    34

---
+++ P37.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class P37 {
+	public static void main(String[] args) {
+		char ch='a';
+		int a=1;
+		for (int i = 1; i <=4; i++) {
+			for (int j = 1; j <=i; j++) {
+				if (i%2==0) {
+					System.out.print(a++);
+				} 
+				else {
+					System.out.print(ch++);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
