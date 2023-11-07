4a56d9bee54cc95b8069a64a340c46e4c5391d7b
chandra swaroop <chandraswaroop6630@gmail.com>
2023-11-07 22:04:17 +0530

    41

---
+++ P314.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class P314 {
+	public static void main(String[] args) {
+		int star=0,space=8;
+		for (int i = 1; i <=4; i++) {
+			star++;
+			space-=2;
+			for (int j = 1; j <=star; j++) {
+				System.out.print('*');
+			}
+			for (int j = 1; j <=space; j++) {
+				System.out.print(' ');
+			}
+			for (int j = 1; j <=star; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+}
