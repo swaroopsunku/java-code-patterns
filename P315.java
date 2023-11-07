2f888e75603023c5c5e83cce03df82ac0ea43bab
chandra swaroop <chandraswaroop6630@gmail.com>
2023-11-07 22:04:25 +0530

    42

---
+++ P315.java
@@ -0,0 +1,22 @@
+package patterns;
+
+public class P315 {
+	public static void main(String[] args) {
+		int star=0,space=4;
+		for (int i = 1; i <=4; i++) {
+			star++;
+			space--;
+			for (int j = 1; j <=star; j++) {
+				System.out.print('*');
+			}
+			for (int j = 1; j <=space; j++) {
+				System.out.print(' ');
+			}
+			for (int j = 1; j <=star; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+	
+}
