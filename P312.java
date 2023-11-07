ec42afbd1b11e2241a0122597866a972e93313a5
chandra swaroop <chandraswaroop6630@gmail.com>
2023-11-07 22:03:37 +0530

    39

---
+++ P312.java
@@ -0,0 +1,13 @@
+package patterns;
+
+public class P312 {
+	public static void main(String[] args) {
+		for (int i = 1; i <=5; i++) {
+			char ch='e';
+			for (int j = 1; j <=i; j++) {
+				System.out.print(ch--);
+			}
+			System.out.println();
+		}
+	}
+}
